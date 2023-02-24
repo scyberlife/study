55a712956e0cbcc3365a6757450e33c7ab8444f9
Sergei Beliaev <s.cyberlife@gmail.com>
2023-02-25 01:28:58 +0600

    fix

--- course_geek/third_month/homework_3/admin.py
+++ course_geek/third_month/homework_3/admin.py
@@ -1,5 +1,5 @@
 from config import dp, bot
-from aiogram import types, Bot
+from aiogram import types
 
 
 async def admin_if(message: types.Message):
@@ -22,9 +22,10 @@ async def proverka(message: types.Message):
         for admin in admins:
             admins_mention += f"@{admin.user.username}, "
         for word in words:
-            if word == message.text.lower():
-                await message.reply(text=f"{admins_mention} Should this person be banned?"
-                                         f"@{message.from_user.username} id:/ {message.from_user.id}")
+            for fraze in message.text.split():
+                if word == fraze.lower():
+                    await message.reply(text=f"{admins_mention} Should this person be banned?"
+                                            f"@{message.from_user.username} id: {message.from_user.id}")
                 break
 
 """
@@ -33,15 +34,16 @@ there are any words from the list.
 """
 
 async def ban_user(message: types.Message):
-    admin_author = await admin_if(message)
-    user_id = int(message.reply_to_message["text"].split('/')[1])
-    if admin_author:
-        try:
-            await bot.ban_chat_member(chat_id=message.chat.id,
-                                      user_id=user_id)
-            await message.answer(f"Пользователь с ID {user_id} забанен.")
-        except Exception as e:
-            await message.answer(f"Не удалось забанить пользователя с ID {user_id}. Ошибка: {e}")
+    if message.reply_to_message:
+        admin_author = await admin_if(message)
+        user_id = int(message.reply_to_message["text"].split(':')[1])
+        if admin_author:
+            try:
+                await bot.ban_chat_member(chat_id=message.chat.id,
+                                        user_id=user_id)
+                await message.answer(f"The user with ID {user_id} has been banned.")
+            except Exception as e:
+                await message.answer(f"Failed to ban user with ID {user_id}. Error: {e}")
 
 """
 The function bans the user.
